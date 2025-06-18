305ba9777346125108b9688dd1a8e419fb55bcbc
triyono <yono.tri@gmail.com>
2025-06-19 05:15:08 +0700

    delete api key supabase

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,6 +1,5 @@
 // lib/utils/constants.dart
 
 // GANTI DENGAN URL DAN ANON KEY DARI PROYEK SUPABASE ANDA
-const String supabaseUrl = 'https://xklpcqxcvdbwixpwcjmg.supabase.co';
-const String supabaseAnonKey =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhrbHBjcXhjdmRid2l4cHdjam1nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkwMTI2NzIsImV4cCI6MjA2NDU4ODY3Mn0.Qhn3FJI81Oi8V4dWHeK8ZHwR5ApDDb9ODezXrF5Hbbo';
+const String supabaseUrl = 'ganti_dengan_URL_dari_supabase';
+const String supabaseAnonKey = 'ganti_dengan_AnonKey_dari_supabase';
