e576ddf5a0c485c50360e0d57bf2d6170acf4a2c
triyono <yono.tri@gmail.com>
2025-06-19 05:22:18 +0700

    update gitignore

--- lib/utils/constants.dart
+++
@@ -1,5 +0,0 @@
-// lib/utils/constants.dart
-
-// GANTI DENGAN URL DAN ANON KEY DARI PROYEK SUPABASE ANDA
-const String supabaseUrl = 'ganti_dengan_URL_dari_supabase';
-const String supabaseAnonKey = 'ganti_dengan_AnonKey_dari_supabase';
