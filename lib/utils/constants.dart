43cc8a8810d7a22ee91d75b62ba419d61f3d968a
triyono <yono.tri@gmail.com>
2025-06-19 09:55:20 +0700

    update constant

---
+++ lib/utils/constants.dart
@@ -0,0 +1,5 @@
+// lib/utils/constants.dart
+
+// GANTI DENGAN URL DAN ANON KEY DARI PROYEK SUPABASE ANDA
+const String supabaseUrl = 'isi_dengan_url_supabase_anda';
+const String supabaseAnonKey = 'isi_dengan_anon_key_dari_proyek_supabase_anda';
