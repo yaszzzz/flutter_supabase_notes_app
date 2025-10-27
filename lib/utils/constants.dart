f5ab6884aee5d07910d3d5ce63f92fd6cc7639d4
Mohamad Ilyas Ali <108250794+yaszzzz@users.noreply.github.com>
2025-10-27 20:54:17 +0700

    commit3

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,5 +1,5 @@
 // lib/utils/constants.dart
 
 // GANTI DENGAN URL DAN ANON KEY DARI PROYEK SUPABASE ANDA
-const String supabaseUrl = 'isi_dengan_url_supabase_anda';
-const String supabaseAnonKey = 'isi_dengan_anon_key_dari_proyek_supabase_anda';
+const String supabaseUrl = 'https://sbondfuorulithmoydzr.supabase.co';
+const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNib25kZnVvcnVsaXRobW95ZHpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA5MDU1MzMsImV4cCI6MjA3NjQ4MTUzM30.-BldkuNilogUWacjswrav-FxjU8IdY_2X8Kbbjc6UDw';
